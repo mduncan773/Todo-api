9be21cec4698ac78387184cd6e4a119f2aba8154
Miles Duncan <miles.c.duncan@gmail.com>
2016-01-29 21:33:35 -0600

    Add POST for todos

--- server.js
+++ server.js
@@ -1,19 +1,14 @@
 var express = require('express');
+//middleware 
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-    id: 1,
-    description: 'Meet mom for lunch',
-    completed: false
-}, {
-    id: 2,
-    description: 'Go to market',
-    completed: false
-},{
-    id: 3,
-    description: 'Walk the dog',
-    completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+// parse any json that comes in 
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
 	res.send('Todo API Root');
@@ -42,6 +37,20 @@ app.get('/todos/:id', function(req, res){
     }
     //res.send('Asking for todo with id of ' + req.params.id);
 });
+
+//POST /todos/
+app.post('/todos', function(req, res){
+    var body = req.body;
+    // add body id, and increment
+    body.id = todoNextId++;
+     todos.push(body);
+    
+    console.log('description: ' + body.description);
+   
+    res.json(body);
+});
+
+// add body to todos array. 
 
 app.listen(PORT, function () {
 	console.log('Express listening on port ' + PORT + '!');
